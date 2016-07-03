ade18bb15fcc86a0ad1ffd3a6e9fb58f162cfd81
LaurensDeV <devoogdlaurens@hotmail.com>
2016-07-03 22:12:04 +0200

    Initial Commit

---
+++ TKicker/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerHasMoved
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "TKicker";
+			int Port = args.GetParam<int>(new string[] { "-p", "-port" }, 7777);
+			string KickMsg = args.GetParam<string>(new string[] { "-m", "-message" }, "Server moved.");
+			string address = args.GetParam<string>("-ip", "0.0.0.0");
+
+			TcpListener listener = new TcpListener(IPAddress.Parse(address), Port);
+			try
+			{
+				listener.Start();
+			}
+			catch (SocketException ex)
+			{
+				if (ex.ErrorCode == 10048)
+					Console.WriteLine($"Address ({listener.LocalEndpoint}) is already in use!");
+				else
+					Console.WriteLine(ex.ToString());
+				Console.WriteLine("Press any key to exit.");
+				Console.ReadKey();
+				return;
+			}
+			Console.WriteLine($"Listening on {listener.LocalEndpoint} with message \"{KickMsg}\".");
+			new Thread(() =>
+			{
+				for (;;)
+				{
+					try
+					{
+						Socket sck = listener.AcceptSocket();
+						Console.WriteLine($"[{DateTime.UtcNow.ToShortDateString()} - {DateTime.UtcNow.ToLongTimeString()}] {sck.RemoteEndPoint} tried to connect.");
+						using (BinaryWriter bw = new BinaryWriter(new MemoryStream()))
+						{
+							bw.Write((short)(KickMsg.Length + 4));
+							bw.Write((byte)2);
+							bw.Write(KickMsg);
+							byte[] res = ((MemoryStream)bw.BaseStream).ToArray();
+							sck.Send(res, 0, res.Length, SocketFlags.None);
+							sck.Disconnect(false);
+						}
+					}
+					catch { }
+				}
+			}).Start();
+		}
+	}
+}
---
+++ TKicker/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ServerHasMoved
+{
+	public static class Utils
+	{
+		public static T GetParam<T>(this string[] args, string find, object Default = default(object))
+		{
+			return args.GetParam<T>(new string[] { find }, Default);
+		}
+
+		public static T GetParam<T>(this string[] args, string[] find, object Default = default(object))
+		{
+			for (int i = 0; i < args.Length; i++)
+			{
+				for (int j = 0; j < find.Length; j++)
+				{
+					if (args[i] == find[j])
+					{
+						if (args.Length < i + 1)
+							break;
+						return (T)Convert.ChangeType(args[i + 1], typeof(T));
+					}
+				}
+			}
+			return (T)Convert.ChangeType(Default, typeof(T));
+		}
+	}
+}
